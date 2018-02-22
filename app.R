b320b288d4bdd4acdcfe4178c3e808cc6d5067cf
glickmac <stronglab2@stronglab2s-MacBook-Pro.local>
2018-02-22 14:11:13 -0700

    server client working

--- app.R
+++ app.R
@@ -1,11 +1,11 @@
 library(shiny)
 library(shinydashboard)
 library(shinyjs)
-library(ggplot2)
+library(shinyFiles)
 ## Load in Data Structure
 df = read.table("data/merged_frames.RData")
 
-
+## Java Script to allow user to hit Enter
 jscode <- '
 $(function() {
   var $els = $("[data-proxy-click]");
@@ -28,29 +28,116 @@ ui = dashboardPage(
   dashboardHeader(title = "GRAB"),
   dashboardSidebar(checkboxGroupInput("assembly", label = h3("Assembly Level"), 
                                   choices = list("Complete Genomes" = "Complete Genome", "Scaffolds" = "Scaffold", "Chromosomes" = "Chromosome", "Contigs" = "Contig"),
-                                  selected = "Complete Genomes"),hr(),fluidRow(column(3, verbatimTextOutput("value")),
-               selectInput("taxonomy", "Select Taxonomic Level", choices = c("Phylum", "Class", "Order", "Family", "Genus", "Species", "Subspecies"))
+                                  selected = "Complete Genome"),hr(),fluidRow(column(3, verbatimTextOutput("value")),
+               selectInput("taxonomy", "Select Taxonomic Level", choices = c("Phylum", "Class", "Order", "Family", "Genus", "Species", "Subspecies")),
+               selectInput("material", "Select Type of Genetic Information", choices = c("Genome", "Coding", "Protein"))
                )),
   dashboardBody(
     useShinyjs(),
+    h4("Press Go! to view table filtered by taxonomic query"),
     actionButton("goButton", "Go!"),
+    
     tags$head(tags$script(HTML(jscode))),
-    tagAppendAttributes(textInput("query", label = h3("Input Organismal Query"), value = ""), `data-proxy-click` = "goButton"),
-    h4("Please input organismal names seperated by either commas or spaces")
+    h3("Input Organismal Query"),
+    h4("Please input organismal names seperated by either commas or spaces"),
+    tagAppendAttributes(textInput("query", label = "", value = ""), `data-proxy-click` = "goButton"),
+    
+    ## Hide certain buttons to prevent out of order
+    hidden(
+    actionButton("down", "Download Sequences to Server"),
+    downloadButton("downloadData", "Download Sequences to Local"),
+    tableOutput("tabs")
     )
-)
+  
+  
+))
 
 server = function(input, output, session) {
-  observeEvent(input$goButton, {
+
+    values = reactiveValues(df_data = NULL)
+
+    observeEvent(input$goButton, {
+      tax = tolower(input$taxonomy)
+      ref = input$assembly
+      query = tolower(input$query)
+      
+      ## Split Query
+      my_list = strsplit(query, split = c(" ", ","))
+      
+      ## Filter data table by Query
+      matches <- df[grep(paste(my_list,collapse="|"),df[,tax], ignore.case = T),]
+      
+      ## filter by checkbox level
+      values$df_data <- matches[grep(paste(ref,collapse="|"),matches[,"assembly_level"], ignore.case = T),]
+      
+      show("down")
+      show("tabs")
+    })
+  
     
     
-    system("wget 'ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/007/865/GCA_000007865.1_ASM786v1/GCA_000007865.1_ASM786v1_protein.faa.gz'")
-  })
-  #x = strsplit(input$query, split = ",")
-  
-  
-  # Downloadable csv of selected dataset ----
-  #output$downloadData <- "Hello World"
+    output$tabs <- renderTable({values$df_data})
+    
+    observeEvent(input$down, {
+      mat = input$material
+      print(input$query)
+      temp = values$df_data
+      
+      calls = as.character(temp$ftp_path)
+      
+      call_base = basename(calls)
+      test = paste(calls,call_base, sep = '/')
+      
+      
+      if(mat == 'Protein'){
+        temp = paste(test, "protein.faa.gz'",sep = "_")
+        wpath = paste("wget ", temp, sep = "'")
+      } else if(mat == 'Coding'){
+        temp = paste(test, "cds_from_genomic.fna.gz'", sep = "_")
+        wpath = paste("wget ", temp, sep = "'")
+      } else{
+        temp = paste(test, "genomic.fna.gz'", sep = "_")
+        wpath = paste("wget ", temp, sep = "'")
+      }
+      
+      if (file.exists("/srv/connect/apps/GRAB/GRAB_Sequences")){
+        system("rm -r -f /srv/connect/apps/GRAB/GRAB_Sequences")
+        system("mkdir /srv/connect/apps/GRAB/GRAB_Sequences")
+      } else {
+        system("mkdir /srv/connect/apps/GRAB/GRAB_Sequences")
+      }
+      
+      setwd("./GRAB_Sequences")
+      lapply(wpath, system)
+      setwd("..")
+      system("zip -r GRAB_Sequences.zip GRAB_Sequences")
+      show('downloadData')
+      
+    })
+    
+    output$downloadData <- downloadHandler(
+      filename <- function() {
+        paste("GRAB_Download", "zip", sep=".")
+      },
+      
+      content <- function(file) {
+        file.copy("/srv/connect/apps/GRAB/GRAB_Sequences.zip", file)
+      },
+      contentType = "application/zip"
+    )
+    
+    ## Adding File System
+    observe({
+      if (file.exists("/srv/connect/apps/GRAB/GRAB_Sequences")){
+        system("rm -r -f /srv/connect/apps/GRAB/GRAB_Sequences")
+      } else{}
+      
+      if (file.exists("/srv/connect/apps/GRAB/GRAB_Sequences.zip")){
+        system("rm -r -f /srv/connect/apps/GRAB/GRAB_Sequences.zip")
+      } else{}
+      
+    })
+    
 }
 
 shinyApp(ui = ui, server = server)
